b291ba6043ea484b3d484f7a7a1634b6a50fa320
Adlvkyn <n.adilakyn01@mail.ru>
2022-09-28 16:45:37 +0600

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className='nav'>
+      <div>
+        <a href="" className="messages">Messages</a>
+      </div>
+      <div>
+        <a href="" className="news">News</a>
+      </div>
+      <div>
+        <a href="" className="music">Music</a>
+      </div>
+      <div>
+        <a href="" className="settings">Settings</a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://tengrinews.kz/userdata/news/2022/news_471474/thumb_m/photo_399549.jpeg" />
+      </div>
+      <div>
+        <img src="https://cdn.dribbble.com/userupload/3158902/file/original-7c71bfa677e61dea61bc2acd59158d32.jpg?resize=400x0" />
+      </div>
+      <div>
+        my posts
+        <div>
+          new post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
